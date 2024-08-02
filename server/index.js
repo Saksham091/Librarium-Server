d2e5eaa53f6dc7b8d14035c9351a393bf99a5042
Saksham091 <saksham.agarwal@kalvium.community>
2024-08-02 14:41:36 +0530

    Payment Feature Added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const auth = require("./Routes/auth")
 const book = require("./Routes/book")
+const payment = require("./Routes/payment")
 const wishlist = require("./Routes/wishlist")
 const cart = require("./Routes/cart")
 const cors = require('cors');
@@ -14,6 +15,7 @@ app.use("/auth", auth)
 app.use("/book", book)
 app.use("/wishlist", wishlist)
 app.use("/cart", cart)
+app.use("/payment", payment)
 
 mongoose.connect(process.env.MONGO_URL).then( () =>{
     app.listen(process.env.PORT, () =>{
